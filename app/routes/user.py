877020d00b372960ca3014a47b73d60916e3e10b
Yaash Jain <yaash.jain45@gmail.com>
2025-10-29 23:56:44 +0530

    Add user PUT endpoint

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,10 +1,13 @@
+from uuid import UUID
+
+from fastapi.exceptions import HTTPException
 from fastapi.routing import APIRouter
 
 from ..models.user import User, UserCreate
 
 router = APIRouter()
 
-users: list[User] = []
+users: dict[UUID, User] = {}
 
 
 @router.get("/", response_model=list[User])
@@ -15,7 +18,7 @@ def get_all_users() -> list[User]:
     Returns:
         list[User]: A list of all users.
     """
-    return users
+    return list(users.values())
 
 
 @router.post("/", response_model=User, status_code=201)
@@ -30,6 +33,37 @@ def create_user(request: UserCreate) -> User:
         User: The newly created user
     """
     new_user = User(**request.model_dump())
-    users.append(new_user)
+    users[new_user.id] = new_user
 
     return new_user
+
+
+@router.put("/{id}", response_model=User)
+def update_user(id: UUID, request: UserCreate) -> User:
+    """
+    Update an existing user.
+
+    Args:
+        id (UUID): The ID of the user to update.
+        request (UserCreate): The new user data.
+
+    Returns:
+        User: The updated user.
+
+    Raises:
+        HTTPException: If the user with the given ID is not found.
+    """
+    user = users.get(id, None)
+
+    if user is None:
+        raise HTTPException(status_code=404, detail=f"Product {id} not found")
+
+    kwargs = request.model_dump()
+    kwargs["id"] = user.id
+    kwargs["created"] = user.created
+
+    updated_user = User(**kwargs)
+
+    users[user.id] = updated_user
+
+    return updated_user
