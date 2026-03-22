f2261dc1c7db4ba7d19855b191505f5296bd2f63
Yaash Jain <yaash.jain45@gmail.com>
2026-03-22 01:39:49 -0700

    Add timezone validation to TimeStamped pydantic model

--- app/core/models.py
+++ app/core/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timezone
 from uuid import uuid4
 
-from pydantic import UUID4, BaseModel, Field
+from pydantic import UUID4, BaseModel, Field, field_validator
 
 
 class Identifiable(BaseModel):
@@ -11,3 +11,16 @@ class Identifiable(BaseModel):
 class TimeStamped(BaseModel):
     created: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     modified: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+
+    @field_validator("*", mode="before")
+    @classmethod
+    def ensure_utc(cls, v):
+        # The sqlite database does not store datetimes with the trailing 'Z'.
+        #
+        # The field validator fixes this by ensuring that any TimeStamped model
+        # returned has consistent utc based datetime values.
+        if isinstance(v, datetime):
+            if v.tzinfo is None:
+                return v.replace(tzinfo=timezone.utc)
+            return v.astimezone(timezone.utc)
+        return v
